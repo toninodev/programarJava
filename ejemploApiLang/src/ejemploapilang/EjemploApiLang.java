3e23a49a2de32fde80a13a3b17aaa233da9a4fd6
Toninogit config --global user.name Toninogit config --global user.name Tonino <antonionunezsantacruz@gmail.com>
2023-11-09 11:49:09 +0100

    Añadido ejemploApiLang

---
+++ ejemploApiLang/src/ejemploapilang/EjemploApiLang.java
@@ -0,0 +1,18 @@
+
+package ejemploapilang;
+import java.lang.*; // De esta forma añadimos la utilidad completa de de la API "lang" 
+// Aqui podremos consultar la documentacion de la API lang : https://docs.oracle.com/javase/8/docs/api/java/lang/package-summary.html
+
+
+public class EjemploApiLang {
+
+    
+    public static void main(String[] args) {
+        double raiz = Math.sqrt(16); // Con el metodo Math.sqrt de la API "lang" podemos sacar la raiz cuadrada del numero que le inidquemos
+        System.out.println(raiz);
+        int numeroAbs = Math.abs(50); // Con el metodo Math.abs podemos sacar el numero absoluto del numero que le indiquemos. 
+        System.out.println(numeroAbs);
+        
+    }
+    
+}
